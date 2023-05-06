49d9cff1065ed8e9f3ee4afbe047b92705c86131
Sergio Arturo <119349223+sergioarturola@users.noreply.github.com>
2023-05-06 11:26:09 -0600

    invertir cadena usando solamente ciclos iterativos

---
+++ InvertirPalabra.cs
@@ -0,0 +1,25 @@
+﻿using System;
+/*
+ * Sin usar metodos y solamente bucles invierta el contenido de un string, 
+ * pida la cadena por teclado
+ */
+namespace Invertir_palabra_sin_metodos
+{
+    class InvertirPalabra
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe la cadena a invertir: ");
+            string cadena = Console.ReadLine();
+            string inverso = "";
+
+            foreach(var letra in cadena)
+            {
+                 inverso = letra + inverso;
+            }
+
+            Console.WriteLine(inverso);
+
+        }
+    }
+}
