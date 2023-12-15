c1593d2971d47e0789dfa59a3a5445f888ca479b
Sergio Arturo <119349223+sergioarturola@users.noreply.github.com>
2023-12-15 11:55:38 -0600

    encontrar palabra de una frase

---
+++ Palabra-mas-grande.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Dada una frase ingresada por teclado, encontrar la palabra mas grande si hay empate
+ * devolver la primer concurrencia
+ */
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa una frase: ");
+        string frase = Console.ReadLine();
+        string[] frase_cortada = frase.Split(' ');
+        List<int> taman = new List<int>();
+
+        foreach(var palabras in frase_cortada)
+        {
+            taman.Add(palabras.Length);
+        }
+
+        int mayor = taman.Max();
+        int indice = taman.IndexOf(mayor);
+        string palabra_grande = frase_cortada[indice];
+
+        Console.WriteLine("La palabra mas grande es {0}", palabra_grande);
+        Console.ReadKey();
+
+
+    }
+}
